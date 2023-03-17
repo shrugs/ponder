30a0762b1a4d0c2c4a97cf7e47fedf260d4a0760
Olias <0xolias@gmail.com>
2023-03-17 14:16:22 -0400

    fix: model types for entities with only id field

--- packages/core/src/codegen/buildEntityTypes.ts
+++ packages/core/src/codegen/buildEntityTypes.ts
@@ -17,89 +17,101 @@ const gqlScalarToTsType: Record<string, string | undefined> = {
 export const buildEntityTypes = (entities: Entity[]) => {
   const entityModelTypes = entities
     .map((entity) => {
-      return `
-   export type ${entity.name}Instance = {
-    ${entity.fields
-      .map((field) => {
-        switch (field.kind) {
-          case FieldKind.ID: {
-            return `${field.name}: string;`;
-          }
-          case FieldKind.ENUM: {
-            return `${field.name}${field.notNull ? "" : "?"}: ${field.enumValues
-              .map((val) => `"${val}"`)
-              .join(" | ")};`;
-          }
-          case FieldKind.SCALAR: {
-            const scalarTsType = gqlScalarToTsType[field.baseGqlType.name];
-            if (!scalarTsType) {
-              throw new Error(
-                `TypeScript type not found for scalar: ${field.baseGqlType.name}`
-              );
-            }
+      const instanceType = `
+        export type ${entity.name}Instance = {
+          ${entity.fields
+            .map((field) => {
+              switch (field.kind) {
+                case FieldKind.ID: {
+                  return `${field.name}: string;`;
+                }
+                case FieldKind.ENUM: {
+                  return `${field.name}${
+                    field.notNull ? "" : "?"
+                  }: ${field.enumValues.map((val) => `"${val}"`).join(" | ")};`;
+                }
+                case FieldKind.SCALAR: {
+                  const scalarTsType =
+                    gqlScalarToTsType[field.baseGqlType.name];
+                  if (!scalarTsType) {
+                    throw new Error(
+                      `TypeScript type not found for scalar: ${field.baseGqlType.name}`
+                    );
+                  }
 
-            return `${field.name}${field.notNull ? "" : "?"}: ${scalarTsType};`;
-          }
-          case FieldKind.LIST: {
-            // This is trash
-            let tsBaseType: string;
-            if (
-              Object.keys(gqlScalarToTsType).includes(
-                field.baseGqlType.toString()
-              )
-            ) {
-              const scalarTypeName = field.baseGqlType.toString();
-              const scalarTsType = gqlScalarToTsType[scalarTypeName];
-              if (!scalarTsType) {
-                throw new Error(
-                  `TypeScript type not found for scalar: ${scalarTypeName}`
-                );
+                  return `${field.name}${
+                    field.notNull ? "" : "?"
+                  }: ${scalarTsType};`;
+                }
+                case FieldKind.LIST: {
+                  // This is trash
+                  let tsBaseType: string;
+                  if (
+                    Object.keys(gqlScalarToTsType).includes(
+                      field.baseGqlType.toString()
+                    )
+                  ) {
+                    const scalarTypeName = field.baseGqlType.toString();
+                    const scalarTsType = gqlScalarToTsType[scalarTypeName];
+                    if (!scalarTsType) {
+                      throw new Error(
+                        `TypeScript type not found for scalar: ${scalarTypeName}`
+                      );
+                    }
+                    tsBaseType = scalarTsType;
+                  } else if (
+                    // @ts-ignore
+                    field.baseGqlType.astNode?.kind ===
+                    Kind.ENUM_TYPE_DEFINITION
+                  ) {
+                    const enumValues = // @ts-ignore
+                      (field.baseGqlType.astNode?.values || []).map(
+                        // @ts-ignore
+                        (v) => v.name.value
+                      );
+                    tsBaseType = `(${enumValues
+                      // @ts-ignore
+                      .map((v) => `"${v}"`)
+                      .join(" | ")})`;
+                  } else {
+                    throw new Error(
+                      `Unable to generate type for field: ${field.name}`
+                    );
+                  }
+
+                  if (!field.isListElementNotNull) {
+                    tsBaseType = `(${tsBaseType} | null)`;
+                  }
+
+                  return `${field.name}${
+                    field.notNull ? "" : "?"
+                  }: ${tsBaseType}[];`;
+                }
+                case FieldKind.RELATIONSHIP: {
+                  return `${field.name}: string;`;
+                }
               }
-              tsBaseType = scalarTsType;
-            } else if (
-              // @ts-ignore
-              field.baseGqlType.astNode?.kind === Kind.ENUM_TYPE_DEFINITION
-            ) {
-              // @ts-ignore
-              const enumValues = (field.baseGqlType.astNode?.values || []).map(
-                // @ts-ignore
-                (v) => v.name.value
-              );
-              // @ts-ignore
-              tsBaseType = `(${enumValues.map((v) => `"${v}"`).join(" | ")})`;
-            } else {
-              throw new Error(
-                `Unable to generate type for field: ${field.name}`
-              );
-            }
+            })
+            .join("")}
+      };`;
 
-            if (!field.isListElementNotNull) {
-              tsBaseType = `(${tsBaseType} | null)`;
-            }
+      const instanceWithoutIdType =
+        entity.fields.length > 1
+          ? `Omit<${entity.name}Instance, "id">`
+          : `Record<string, never>`;
 
-            return `${field.name}${field.notNull ? "" : "?"}: ${tsBaseType}[];`;
-          }
-          case FieldKind.RELATIONSHIP: {
-            return `${field.name}: string;`;
-          }
-        }
-      })
-      .join("")}
-  };
+      const modelType = `
+        export type ${entity.name}Model = {
+          get: (id: string) => Promise<${entity.name}Instance | null>;
+          insert: (id: string, obj: ${instanceWithoutIdType}) => Promise<${entity.name}Instance>;
+          update: (id: string, obj: Partial<${instanceWithoutIdType}>) =>
+            Promise<${entity.name}Instance>;
+          delete: (id: string) => Promise<boolean>;
+          upsert: (id: string, obj: ${instanceWithoutIdType}) => Promise<${entity.name}Instance>;
+        };
+      `;
 
-  export type ${entity.name}Model = {
-    get: (id: string) => Promise<${entity.name}Instance | null>;
-    insert: (id: string, obj: Omit<${entity.name}Instance, "id">) => Promise<${
-        entity.name
-      }Instance>;
-    update: (id: string, obj: Partial<Omit<${entity.name}Instance, "id">>) =>
-      Promise<${entity.name}Instance>;
-    delete: (id: string) => Promise<boolean>;
-    upsert: (id: string, obj: Omit<${entity.name}Instance, "id">) => Promise<${
-        entity.name
-      }Instance>;
-  };
-    `;
+      return instanceType + modelType;
     })
     .join("");
 
